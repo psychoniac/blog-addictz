6174ea6a7a77684ea7c66d76cfec0889b443f9c0
jlnko <jlnko@hotmail.fr>
2024-11-12 15:33:19 +0100

    Création des cards post qui emmene vers les pages posts.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,18 +1,22 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import { graphql, Link } from "gatsby";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
+import { contenerPosts, cardPost } from "./blogPost.module.css";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Mes posts">
       <h2>Mes derniers posts</h2>
-      <div>
+      <div className={contenerPosts}>
         {data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h3>{node.frontmatter.title}</h3>
+          <article key={node.id} className={cardPost}>
+            <h3>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h3>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))}
       </div>
@@ -25,8 +29,9 @@ export const query = graphql`
     allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
         frontmatter {
-          title
           date(formatString: "MMMM D, YYYY")
+          title
+          slug
         }
         id
         excerpt
